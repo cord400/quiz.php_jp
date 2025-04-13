21e443ea4c7e48d5495f736e2ea4978488f32fee
cord400 <cord08681@gmail.com>
2025-04-13 10:24:07 +0900

    Update index.php

--- xxx/admin/index.php
+++ xxx/admin/index.php
@@ -3,27 +3,21 @@ session_start();
 require_once('../config/db.php');
 
 if (!isset($_SESSION['user']) || $_SESSION['role_rank'] < 1) {
-    header('Location: ../account/login.php');
-    exit;
+  header('Location: ../account/login.php');
+  exit;
 }
-?>
 
+?>
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <title>管理者ページ</title>
-</head>
+<head><meta charset="UTF-8"><title>管理者ホーム</title></head>
 <body>
-  <h1>管理メニュー</h1>
-  <ul>
-    <li><a href="add_quiz.php">クイズを追加</a></li>
-    <li><a href="archive.php">クイズのアーカイブ</a></li>
-    <li><a href="cord.php">ユーザー正解数確認（QR/コード）</a></li>
-    <?php if ($_SESSION['role_rank'] >= 2): ?>
-      <li><a href="add_user.php">管理者アカウント作成</a></li>
-    <?php endif; ?>
-    <li><a href="../quiz.php">クイズページへ</a></li>
-  </ul>
+<h1>🔒 管理者ホーム</h1>
+<ul>
+  <li><a href="add_quiz.php">クイズ作成</a></li>
+  <li><a href="archive.php">アーカイブ一覧</a></li>
+  <li><a href="cord.php">ユーザー正解数確認</a></li>
+  <li><a href="add_user.php">管理者アカウント作成</a></li>
+</ul>
 </body>
 </html>
